5978965c59915dc805ea6dded30f802369002c90
“Klammertime” <audrey.ck@gmail.com>
2016-01-23 17:34:16 -0800

    Player and enemies on screen. When player hit by enemy, goes back to start.

--- js/app.js
+++ js/app.js
@@ -1,75 +1,134 @@
-// Enemies our player must avoid
 var Enemy,
     enemy,
     Player,
     player,
     allEnemies = [];
 
-
-Enemy = function(enemyX, enemyY, enemySpeed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    // I have to set initial location
+Enemy = function(enemyX, enemyY) {
+    // Image/sprite for enemies, uses a helper provided in resources.js
+    this.sprite = "images/enemy-bug.png";
     this.x = enemyX;
     this.y = enemyY;
-
-    // set initial speed
-    this.speed = enemySpeed;
+    this.isMoving = true;
+    this.won = false;
+    // Sets initial speed.
+    this.speed = Math.floor((Math.random() * 400) + 50);
+    // this.direction = "right";
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Updates enemy's position using dt param, a time delta between ticks.
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    if (this.x < 550) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -(Math.floor(Math.random() * 50) + 30);
+    }
+    // Multiply movement by dt ensures game runs at the same speed for
     // all computers.
+    if (player.x >= this.x - 40 && player.x <= this.x + 40) {
+        if (player.y >= this.y - 20 && player.y <= this.y + 20) {
+            player.reset(this.player);
+        }
+    }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draws enemy on screen.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Player class.
 Player = function() {
-    this.sprite = 'images/char-boy.png';
+    this.sprite = "images/char-boy.png";
     this.x = 200;
-    this.y = 420;
-    this.speed = 0.2;
+    this.y = 380;
+    this.width = 101;
+    this.height = 171;
     this.isMoving = false;
-};
-
-Player.prototype.update = function(dt) {
-
+    this.direction = "up";
+    this.won = false;
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-// This class requires an update(), render() and
-// a handleInput() method.
-Player.prototype.handleInput = function() {
-
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-for(var i = 0; i < 10; i++) {
-    var speed = Math.random() * 0.6;
-    enemy = new Enemy(-100, 60, speed);
-    allEnemies.push(enemy);
+// this didn't work, it doesn't erase easily
+Player.prototype.win = function() {
+    ctx.font = "46px Orbitron";
+    ctx.fillText("You Win!!!", 140, 40);
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 380;
+    this.won = false;
+    this.isMoving = false;
+};
+
+Player.prototype.update = function() {
+    var futureX = this.x,
+        futureY = this.y;
+
+    if (this.isMoving) {
+        switch (this.direction) {
+            case "up":
+                if (futureY === 60 && this.won === false) {
+                    futureY = futureY - 59;
+                } else {
+                    futureY = futureY - 80;
+                }
+                break;
+            case "right":
+                futureX = futureX + 100;
+                break;
+            case "down":
+                futureY = futureY + 80;
+                break;
+            case "left":
+                futureX = futureX - 100;
+                break;
+        }
+        if (futureY > 402 || futureY < 0) {
+            this.isMoving = true;
+            return false;
+        } else if (futureX > 400 || futureX < 0) {
+            this.isMoving = false;
+            return false;
+        } else if (futureY === 1) {
+            this.y = futureY;
+            this.won = true;
+            // this.win();
+
+            // Let player reach water then reset after 1 second delay.
+            setTimeout(function() {
+                this.player.reset();
+            }, 1000);
+            // TODO: something for winning?
+            console.log("you win");
+        } else {
+            this.x = futureX;
+            this.y = futureY;
+            this.isMoving = false;
+        }
+    }
+};
+
+Player.prototype.handleInput = function(keyDirection) {
+    this.isMoving = true;
+    this.direction = keyDirection;
+};
+
+for (var i = 0; i < 4; i++) {
+    var tempY = [60, 150, 220, 150];
+    allEnemies.push(new Enemy(-2, tempY[i]));
 }
 
+// TODOLAST: make different players based on dif chars
 // Place the player object in a variable called player
 player = new Player();
-// TODOLAST: make different players based on dif chars
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to player.handleInput
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
